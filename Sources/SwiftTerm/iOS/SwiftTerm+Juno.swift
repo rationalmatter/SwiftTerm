f1966050e5b387663d6d359e4be9ca871fa351d3
Alex Staravoitau <git@use.fastmail.com>
2023-10-09 19:01:29 +0100

    Add wraparound-invariant position calculations

--- Sources/SwiftTerm/iOS/SwiftTerm+Juno.swift
+++ Sources/SwiftTerm/iOS/SwiftTerm+Juno.swift
@@ -5,6 +5,8 @@
 import Foundation
 
 public extension TerminalView {
+
+    /// Makes sure the UI is updated correctly after terminal manipulations in the `handler`.
     func dispatchTerminalUpdates(_ handler: (Terminal) -> Void) {
         feedPrepare()
         handler(getTerminal())
@@ -18,6 +20,8 @@ public extension TerminalView {
 }
 
 public extension Terminal {
+
+    /// Retrieves contents of the line, starting with specified position, also appending any rows with wrapped parts of the current line.
     func getWrappedLineCharData(scrollInvariantStartingPosition start: Position) -> [CharData] {
         var data: [CharData] = []
         // Append rest of the line on the start row
@@ -28,13 +32,12 @@ public extension Terminal {
             row += 1
             data += buffer.lines[row].data
         }
-        // Trim trailing empty cells
-        while let last = data.last, last.code == 0 {
-            data.removeLast()
-        }
+        // Remove all empty cells
+        data = data.filter { $0.code != 0 }
         return data
     }
-
+    
+    /// Erases cells to the end of the current line, starting with specified position, also clearing any rows with wrapped parts of the current line.
     func clearWrappedLine(scrollInvariantStartingPosition start: Position) {
         // Clear rest of the line on the start row
         eraseInBufferLine(y: start.row - buffer.yDisp, start: start.col, end: cols)
@@ -46,14 +49,17 @@ public extension Terminal {
         }
     }
 
+    /// Retrieves cursor position counting from the beginning of buffer, not from the first visible row.
     func getScrollInvariantCursorPosition() -> Position {
         Position(col: buffer.x, row: buffer.yDisp + buffer.y)
     }
-
+    
+    /// Sets cursor position counting from the beginning of buffer, not from the first visible row.
     func setScrollInvariantCursorPosition(_ position: Position) {
         setCursor(col: position.col, row: position.row - buffer.yDisp)
     }
-
+    
+    /// Feeds byte array, making sure that the cursor is carried over to the next line, if the data happened to fill the current row to the end.
     func feedWithCursorWraparound (byteArray: [UInt8]) {
         feed(byteArray: byteArray)
         // Make sure the cursor is not on the end of the line
@@ -63,5 +69,53 @@ public extension Terminal {
             clearWrappedLine(scrollInvariantStartingPosition: newCursorPosition)
             setScrollInvariantCursorPosition(newCursorPosition)
         }
+    }
+
+    /// Calculates wraparound-invariant position in buffer — that is, position as if there was no wrapping, and every line had as many columns as it needed — based on a position in buffer with wrapped lines. Both positions are scroll-invariant, i.e. counting from the beginning of buffer, not from the first visible row.
+    func getWraparoundInvariantPosition(forBufferPosition position: Position) -> Position? {
+        guard position.row > 0 && position.row < buffer.lines.count else { return nil }
+        guard position.row != 0 else { return position }
+
+        var currentWraparoundInvariantRow = 0
+        var currentWrappedRows = 0
+        var currentBufferRow = 1
+        while currentBufferRow <= position.row {
+            if buffer.lines[currentBufferRow].isWrapped {
+                currentWrappedRows += 1
+            }
+            else {
+                currentWraparoundInvariantRow += 1
+                currentWrappedRows = 0
+            }
+            currentBufferRow += 1
+        }
+        return Position(col: position.col + currentWrappedRows * cols, row: currentWraparoundInvariantRow)
+    }
+    
+    /// Calculates position in buffer with wrapped lines, based on a wraparound-invariant position in buffer — that is, position as if there was no wrapping, and every line had as many columns as it needed. Both positions are scroll-invariant, i.e. counting from the beginning of buffer, not from the first visible row.
+    func getBufferPosition(forWraparoundInvariantPosition position: Position) -> Position? {
+        var currentBufferRow = 0
+        var currentWraparoundInvariantRow = 0
+        while currentBufferRow < buffer.lines.count {
+            if !buffer.lines[currentBufferRow].isWrapped && currentBufferRow != 0 {
+                currentWraparoundInvariantRow += 1
+            }
+            // If we've reached the wraparound-invariant row we're looking for...
+            if currentWraparoundInvariantRow == position.row {
+                let additionalWrappedCols = position.col % cols
+                let additionalWrappedRows = position.col / cols
+                // Check if we're trying to get a position beyond available lines
+                if currentBufferRow + additionalWrappedRows >= buffer.lines.count {
+                    return nil
+                }
+                // If the line we land on isn't wrapped, but we have additional wrapped rows, it's invalid
+                if !buffer.lines[currentBufferRow + additionalWrappedRows].isWrapped && additionalWrappedRows > 0 {
+                    return nil
+                }
+                return Position(col: additionalWrappedCols, row: currentBufferRow + additionalWrappedRows)
+            }
+            currentBufferRow += 1
+        }
+        return nil
     }
 }
